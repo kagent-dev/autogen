59dea5944bb0f130bd1bf7e660467b3a9dbacca6
Eitan Yarmush <eitan.yarmush@solo.io>
2025-03-06 13:49:49 +0000

    revert run-id

--- python/packages/autogen-studio/autogenstudio/datamodel/db.py
+++ python/packages/autogen-studio/autogenstudio/datamodel/db.py
@@ -47,9 +47,7 @@ class Message(SQLModel, table=True):
     session_id: Optional[int] = Field(
         default=None, sa_column=Column(Integer, ForeignKey("session.id", ondelete="CASCADE"))
     )
-    run_id: Optional[UUID] = Field(
-        default=None, sa_column=Column(SQLAlchemyUUID, ForeignKey("run.id", ondelete="CASCADE"))
-    )
+    run_id: Optional[int] = Field(default=None, sa_column=Column(Integer, ForeignKey("run.id", ondelete="CASCADE")))
 
     message_meta: Optional[Union[MessageMeta, dict]] = Field(default={}, sa_column=Column(JSON))
 
@@ -84,7 +82,7 @@ class Run(SQLModel, table=True):
 
     __table_args__ = {"sqlite_autoincrement": True}
 
-    id: UUID = Field(default_factory=uuid4, sa_column=Column(SQLAlchemyUUID, primary_key=True, index=True, unique=True))
+    id: Optional[int] = Field(default=None, primary_key=True)
     created_at: datetime = Field(
         default_factory=datetime.now, sa_column=Column(DateTime(timezone=True), server_default=func.now())
     )
