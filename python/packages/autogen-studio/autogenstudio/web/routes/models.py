4a72b6e2d8e6d934fc08c08c0be581d0a0c6086c
Peter Jausovec <peter.jausovec@solo.io>
2025-04-25 11:51:34 -0700

    include fiunction_calling field for each model
    Signed-off-by: Peter Jausovec <peter.jausovec@solo.io>

--- python/packages/autogen-studio/autogenstudio/web/routes/models.py
+++ python/packages/autogen-studio/autogenstudio/web/routes/models.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Any, Dict, List
 
 from autogen_ext.models.anthropic._model_info import _MODEL_INFO as anthropic_models
 from autogen_ext.models.ollama._model_info import _MODEL_INFO as ollama_models
@@ -9,12 +9,38 @@ router = APIRouter()
 
 
 @router.get("/")
-async def list_models() -> Dict[str, List[str]]:
+async def list_models() -> Dict[str, List[Dict[str, Any]]]:
+    # Get the model names from the model info, also get the  "function_calling" value for each model
+
     response = {
-        "anthropic": list(anthropic_models.keys()),
-        "ollama": list(ollama_models.keys()),
-        "openAI": list(openai_models.keys()),
-        "azureOpenAI": list(openai_models.keys()),
+        "anthropic": [
+            {
+                "name": model,
+                "function_calling": anthropic_models[model]["function_calling"],
+            }
+            for model in anthropic_models.keys()
+        ],
+        "ollama": [
+            {
+                "name": model,
+                "function_calling": ollama_models[model]["function_calling"],
+            }
+            for model in ollama_models.keys()
+        ],
+        "openAI": [
+            {
+                "name": model,
+                "function_calling": openai_models[model]["function_calling"],
+            }
+            for model in openai_models.keys()
+        ],
+        "azureOpenAI": [
+            {
+                "name": model,
+                "function_calling": openai_models[model]["function_calling"],
+            }
+            for model in openai_models.keys()
+        ],
     }
 
     return response
